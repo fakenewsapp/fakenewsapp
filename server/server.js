bcc9e67d70b25654347e17013c4466ef2843ebad
ebeneroff <ebenerof@calpoly.edu>
2017-01-22 02:53:44 -0800

    updated api calls

--- server/server.js
+++ server/server.js
@@ -21,11 +21,11 @@ var parameters = {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 // Allow cross origins requests, needed for local testing
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 
 // ROUTES FOR OUR API
 // ==============================================
